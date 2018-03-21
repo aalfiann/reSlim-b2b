ae21210189876b65f929984a7c45f6e95cba2699
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-03-21 13:44:55 +0700

    - First upload app ui template

---
+++ test/app/navbar-logo.php
@@ -0,0 +1,20 @@
+<!-- ============================================================== -->
+                <!-- Logo -->
+                <!-- ============================================================== -->
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">
+                        <!-- Logo icon --><b>
+                            <!-- You can put here icon as well -->
+                            <i class="mdi mdi-desktop-mac dark-logo"></i>
+                            <i class="mdi mdi-desktop-mac light-logo"></i>
+                        </b>
+                        <!--End Logo icon -->
+                        <!-- Logo text --><span>
+                         <!-- dark Logo text -->
+                         <img src="../assets/images/logo-text.png" class="dark-logo" alt="<?php echo Core::getInstance()->title?>" />
+                         <!-- Light Logo text -->    
+                         <img src="../assets/images/logo-light-text.png" class="light-logo" alt="<?php echo Core::getInstance()->title?>" /></span> </a>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End Logo -->
+                <!-- ============================================================== -->
