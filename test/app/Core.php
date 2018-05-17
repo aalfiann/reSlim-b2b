b3bb49c065afe6c104d4e7670251f936203c3440
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-05-17 15:06:11 +0700

    - Update version

--- test/app/Core.php
+++ test/app/Core.php
@@ -78,7 +78,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '2.8.2';
+        var $version = '2.9.0';
 
         private static $instance;
         
