d66ffcec4e5f570b7a112d7f92c6ba813dcb9268
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-05-28 12:01:58 +0700

    - Update modules

--- src/modules/enterprise/Enterprise.php
+++ src/modules/enterprise/Enterprise.php
@@ -3,7 +3,6 @@
 namespace modules\enterprise;                       //Make sure namespace is same structure with parent directory
 
 use \classes\Auth as Auth;                          //For authentication internal user
-use \classes\JSON as JSON;                          //For handling JSON in better way
 use \classes\CustomHandlers as CustomHandlers;      //To get default response message
 use PDO;                                            //To connect with database
 
@@ -16,43 +15,47 @@ use PDO;                                            //To connect with database
      * @license    https://github.com/aalfiann/reSlim-modules/tree/master/enterprise/LICENSE.md  MIT License
      */
     class Enterprise {
-        // modules information var
-        protected $information = [
-            'package' => [
-                'name' => 'Enterprise',
-                'uri' => 'https://github.com/aalfiann/reSlim-modules/tree/master/enterprise',
-                'description' => 'Enterprise management system',
-                'version' => '1.0',
-                'require' => [
-                    'reSlim' => '1.9.0'
-                ],
-                'license' => [
-                    'type' => 'MIT',
-                    'uri' => 'https://github.com/aalfiann/reSlim-modules/tree/master/enterprise/LICENSE.md'
-                ],
-                'author' => [
-                    'name' => 'M ABD AZIZ ALFIAN',
-                    'uri' => 'https://github.com/aalfiann'
-                ],
-            ]
-        ];
 
         // database var
         protected $db;
+
+        //base var
+        protected $basepath,$baseurl,$basemod;
 
         //master var
 		var $username,$token;
         
         //construct database object
         function __construct($db=null) {
-			if (!empty($db)) {
-    	        $this->db = $db;
-        	}
+			if (!empty($db)) $this->db = $db;
+            $this->baseurl = (($this->isHttps())?'https://':'http://').$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
+            $this->basepath = $_SERVER['DOCUMENT_ROOT'].dirname($_SERVER['PHP_SELF']);
+			$this->basemod = dirname(__FILE__);
+        }
+
+        //Detect scheme host
+        function isHttps() {
+            $whitelist = array(
+                '127.0.0.1',
+                '::1'
+            );
+            
+            if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+                if (!empty($_SERVER['HTTP_CF_VISITOR'])){
+                    return isset($_SERVER['HTTPS']) ||
+                    ($visitor = json_decode($_SERVER['HTTP_CF_VISITOR'])) &&
+                    $visitor->scheme == 'https';
+                } else {
+                    return isset($_SERVER['HTTPS']);
+                }
+            } else {
+                return 0;
+            }            
         }
 
         //Get modules information
         public function viewInfo(){
-            return JSON::encode($this->information,true);
+            return file_get_contents($this->basemod.'/package.json');
         }
 
     }
