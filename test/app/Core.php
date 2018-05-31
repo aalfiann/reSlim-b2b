7539001136b140e772a7d13d49319a78c7aafd45
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-05-31 14:57:26 +0700

    - Update to version 2.10.2

--- test/app/Core.php
+++ test/app/Core.php
@@ -78,7 +78,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '2.10.1';
+        var $version = '2.10.2';
 
         private static $instance;
         
