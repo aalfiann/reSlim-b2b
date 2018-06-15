b5091c5eb5e628c776f4abf2a76996095eb8ffc4
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-06-16 03:23:20 +0700

    - Update app version to 2.10.5

--- test/app/Core.php
+++ test/app/Core.php
@@ -78,7 +78,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '2.10.4';
+        var $version = '2.10.5';
 
         private static $instance;
         
