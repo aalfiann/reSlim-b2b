f6e8a25834b6ba9b3e15128f0569a70d93bdd09a
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-06-25 18:15:42 +0700

    - Update UI version to 2.10.6

--- test/app/Core.php
+++ test/app/Core.php
@@ -78,7 +78,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '2.10.5';
+        var $version = '2.10.6';
 
         private static $instance;
         
