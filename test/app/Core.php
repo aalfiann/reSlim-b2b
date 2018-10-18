c95759691010f579f1e2857d5089b441d285023d
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-10-19 00:07:47 +0700

    Changelog 2.19.0 : - Implemented lazyload PDO - Update reslim version to 1.19.0

--- test/app/Core.php
+++ test/app/Core.php
@@ -87,7 +87,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '2.18.0';
+        var $version = '2.19.0';
 
         private static $instance;
         
