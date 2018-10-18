f09b7a75943590d338875bc91d157df694a1b231
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-10-18 17:31:16 +0700

    Changelog 1.18.0 : - Update version to 1.18.0 - Update readme - Add alphabetOnly() function in classes/Validation.php - Improvement ApiKey middleware - Improvement user.router.php - Update pages, flexibleconfig and enterprise modules

--- test/app/Core.php
+++ test/app/Core.php
@@ -87,7 +87,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '2.17.1';
+        var $version = '2.18.0';
 
         private static $instance;
         
