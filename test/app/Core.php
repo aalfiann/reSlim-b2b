1b625d3440d3d6d0eac8a3cc518247c194cbb129
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-11-22 15:52:04 +0700

    Changelog 2.21.2 : - Fixed auto index number in datatables

--- test/app/Core.php
+++ test/app/Core.php
@@ -87,7 +87,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '2.21.1';
+        var $version = '2.21.2';
 
         private static $instance;
         
