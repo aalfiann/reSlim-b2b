e3ddeb9616cc36650fdacbeabebfc1064178f0da
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-12-03 08:15:43 +0700

    Changelog 2.22.1 : - Update reSlim version to 1.22.1 - Update Core UI version to 2.22.1 - Update readme

--- test/app/Core.php
+++ test/app/Core.php
@@ -87,7 +87,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '2.22.0';
+        var $version = '2.22.1';
 
         private static $instance;
         
