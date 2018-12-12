950566641f64e0293b10c4f3faff7ad5fbdb4649
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-12-13 00:00:20 +0700

    Changelog 2.23.1 : - Cache subdirectory now auto randomize - Update reSlim version to 1.23.1 - Update Core UI version to 2.23.1

--- test/app/Core.php
+++ test/app/Core.php
@@ -87,7 +87,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '2.23.0';
+        var $version = '2.23.1';
 
         private static $instance;
         
