516385cb65e14361ad31f4b45e204bbca527a553
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-12-19 19:22:49 +0700

    Changelog 2.23.2 : - Add example nginx.conf for main or sub directory - Update version to 2.23.2 - Update readme

--- test/app/Core.php
+++ test/app/Core.php
@@ -87,7 +87,7 @@
         var $pathcache = 'cache-files';
         var $minifycache = true;
 
-        var $version = '2.23.1';
+        var $version = '2.23.2';
 
         private static $instance;
         
